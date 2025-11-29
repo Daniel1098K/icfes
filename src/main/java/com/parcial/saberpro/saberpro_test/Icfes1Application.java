1612a2c1b6f81090ec6223b1a1d266a83fede62a
Daniel Perez <80496933+Daniel1098K@users.noreply.github.com>
2025-11-29 05:02:32 -0500

    Remove SpringBootApplication annotation
    Removed the SpringBootApplication annotation from the main application class.

--- src/main/java/com/parcial/saberpro/saberpro_test/Icfes1Application.java
+++ src/main/java/com/parcial/saberpro/saberpro_test/Icfes1Application.java
@@ -3,7 +3,6 @@ package com.parcial.saberpro.saberpro_test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class Icfes1Application {
 
 	public static void main(String[] args) {
