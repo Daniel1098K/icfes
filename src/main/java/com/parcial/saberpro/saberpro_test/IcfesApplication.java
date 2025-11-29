0b8445515388e931c1e9b57309b92eb1999d99cd
Daniel Perez <80496933+Daniel1098K@users.noreply.github.com>
2025-11-29 05:02:48 -0500

    Fix formatting in IcfesApplication.java

--- src/main/java/com/parcial/saberpro/saberpro_test/IcfesApplication.java
+++ src/main/java/com/parcial/saberpro/saberpro_test/IcfesApplication.java
@@ -3,7 +3,7 @@ package com.parcial.saberpro.saberpro_test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class IcfesApplication {
 
 	public static void main(String[] args) {
